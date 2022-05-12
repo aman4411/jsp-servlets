14cc2095bc040600de7de832186392042e080b88
Aman Mehta <amanmehta4411@gmail.com>
2022-05-12 13:01:32 +0530

    first servlet

---
+++ src/com/demoservlet/AddServlet.java
@@ -0,0 +1,17 @@
+package com.demoservlet;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet{
+
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int num1 = Integer.parseInt(req.getParameter("num1"));
+		int num2 = Integer.parseInt(req.getParameter("num2"));
+		int result = num1+num2;
+		res.getWriter().print(result);
+	}
+}
